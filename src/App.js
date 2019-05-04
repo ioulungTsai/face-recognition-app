285831126267128dc6ddc69cf36aec03cdfec750
brandonTsai <brandonya125@gmail.com>
2019-05-03 23:29:20 -0700

    add rank component and particles background

--- src/App.js
+++ src/App.js
@@ -1,14 +1,56 @@
 import React from 'react';
+import Particles from 'react-particles-js';
 import Navigation from './components/Navigation/Navigation'
 import Logo from './components/Logo/Logo'
+import Rank from './components/Rank/Rank'
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
 import './App.css';
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 99,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    },
+    color: {
+      value: '#6495ED'
+    },
+    line_linked: {
+      color: '#66cdaa',
+      shadow: {
+        enable: false
+      }
+    },
+    size: {
+      value: 3,
+      random: true,
+    },
+    move: {
+      enable: true,
+      speed: 3,
+      random: true,
+      out_mode: "out",
+      attract: {
+        enable: false,
+        rotateX: 600,
+        rotateY: 1200
+      }
+    }
+  }
+};
 
 function App() {
   return (
     <div className="App">
+      <Particles className='particles'
+                params={particlesOptions}
+              />
       <Navigation />
       <Logo />
+      <Rank />
       <ImageLinkForm />
       {/* {
       <FaceRecognition />} */}
---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Rank = () => {
+  return (
+    <div>
+      <div className='f3 dark-blue'>
+        {'Brandon, your current rank is...'}
+      </div>
+      <div className='f1 dark-blue'>
+        {'#38'}
+      </div>
+    </div>
+  )
+}
+
+export default Rank;
